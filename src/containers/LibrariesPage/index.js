f74a96b1a1f6dc663e1522639a9ebc6dac14fa51
Svetlana Shumakova <svetlana.shumakova@altexsoft.com>
2015-08-29 19:12:58 +0300

    add table to the library page

--- src/containers/LibrariesPage/index.js
+++ src/containers/LibrariesPage/index.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { loadLibraries } from '../../actions';
+import Table from '../../components/Table/index.js';
+import {boldTextRender} from '../../components/Table/renders.js';
 
-@connect(
+@connect (
   (state) => ({libraries: state.libraries}),
   (dispatch) => bindActionCreators({ loadLibraries}, dispatch)
 )
@@ -15,10 +17,37 @@ export default class LibrariesPage extends Component {
     props.loadLibraries();
   }
 
+    onRowClick(){
+
+    }
+
+    config = {
+        columns: [
+            {
+                key: 'name',
+                render: boldTextRender
+            }, {
+                key: 'folder',
+                icon: 'fa fa-folder-open',
+                text: 'Folder'
+            }, {
+                key: 'media',
+                icon: 'fa fa-file',
+                text: 'Media'
+            }, {
+                key: 'views',
+                icon: 'fa fa-eye',
+                text: 'Views'
+            }
+        ],
+        noCheck: false,
+    }
+
   render() {
     console.log("this.props.libraries",this.props)
     return (<div>
       Librares
+        <Table ref="table" config={this.config} data={this.props.libraries} onRowClick={::this.onRowClick}/>
     </div>);
   }
 }
