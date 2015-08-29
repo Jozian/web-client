9827707fc697d549554f2c1dd43a15e8f390b782
Alex Shestmintsev <shestmintsev@gmail.com>
2015-08-29 17:44:51 +0300

    Load libraries for libraryPage

--- src/containers/LibrariesPage/index.js
+++ src/containers/LibrariesPage/index.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
 
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { loadLibraries } from '../../actions';
+
+@connect(
+  (state) => ({libraries: state.libraries}),
+  (dispatch) => bindActionCreators({ loadLibraries}, dispatch)
+)
 export default class LibrariesPage extends Component {
+
+  constructor(props) {
+    super(props);
+    props.loadLibraries();
+  }
+
   render() {
     return (<div>
       Librares
