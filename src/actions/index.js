839d11b9032b05ea752f1cd2e2a44946b49070e9
Illya Klymov <xanf@xanf.me>
2015-08-29 17:28:56 +0300

    Actually add files from previous commit

---
+++ src/actions/index.js
@@ -0,0 +1,12 @@
+import librariesApi from './api/libraries';
+import * as types from './actions/types';
+
+export function loadLibraries() {
+  return {
+    type: types.CALL_API,
+    payload: {
+      types: [types.LIBRARIES_LOADING, types.LIBRARIES_LOADED, types.LIBRARIES_LOAD_ERROR],
+      promise: librariesApi.getLibrariesList(),
+    },
+  };
+}
