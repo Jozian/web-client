d206025a6a02e23d2060ed6ae67c6fc1166b5c91
apachkov <anatolii.pachkov@gmail.com>
2015-08-29 15:55:10 +0300

    Button ESLint errors fix

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,15 +2,22 @@ import React, { Component } from 'react';
 import style from './index.css';
 import fontAwesome from 'font-awesome-webpack'; // eslint-disable-line no-unused-vars
 
-export default class Button extends Component{
+export default class Button extends Component {
+  static propTypes = {
+    icon: React.PropTypes.string,
+    text: React.PropTypes.string,
+    disabled: React.PropTypes.bool,
+    onClick: React.PropTypes.func,
+  };
+
   render() {
-    let disabled = this.props.disabled ? 'disabled' : '';
+    const disabled = this.props.disabled ? 'disabled' : '';
 
     return (
       <button type="button" className={style.iconButton} onClick={this.props.onClick} disabled={disabled}>
         <i className={this.props.icon}></i>
         {this.props.text}
       </button>
-    )
+    );
   }
 }
