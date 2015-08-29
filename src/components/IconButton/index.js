730bbbd1f6f7e75172e15d92fd3e1e764bf70172
Alex Shestmintsev <shestmintsev@gmail.com>
2015-08-29 12:45:03 +0300

    add IconButton component

---
+++ src/components/IconButton/index.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import styles from './style.css';
+
+class IconButton extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isVisible: false
+    };
+  }
+
+  show(){
+    this.setState({
+      isVisible: true,
+    })
+  }
+
+  hide(){
+    this.setState({
+      isVisible: false,
+    })
+  }
+
+  render() {
+
+    let tooltip;
+    if (this.state.isVisible){
+      tooltip = <div className={styles.tooltip}>text</div>
+    }
+
+    return (
+      <div onMouseEnter={::this.show} onMouseLeave={::this.hide}>
+        <button onClick={this.props.handleClick}>123</button>
+        { tooltip }
+      </div>
+    );
+  }
+}
+
+IconButton.propTypes = {
+  icon: React.PropTypes.string,
+  tooltipText: React.PropTypes.string,
+  handleClick: React.PropTypes.func,
+};
+
+export default IconButton;
