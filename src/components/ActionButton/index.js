562147c43283b54226a742bd1b89ed304f8f9683
Illya Klymov <xanf@xanf.me>
2015-09-07 02:11:52 +0300

    Implement ActionButton component for async tasks blocking

---
+++ src/components/ActionButton/index.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Button from 'components/Button';
+
+export default class ActionButton extends Component {
+  static propTypes = {
+    inProgress: React.PropTypes.bool,
+    disabled: React.PropTypes.bool,
+    icon: React.PropTypes.string,
+    children: React.PropTypes.node,
+  }
+
+  static defaultProps = {
+    disabled: false,
+    icon: null,
+  }
+
+  render() {
+    const disabled = this.props.disabled || this.props.inProgress;
+    const className = this.props.inProgress ? 'fa fa-spin fa-cog' : this.props.icon;
+    return (
+      <Button {...this.props} disabled={disabled} icon={className}>
+        {this.props.children}
+      </Button>
+    );
+  }
+}
