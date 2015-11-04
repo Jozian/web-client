8f5eea1cb4130bd1b2986faca4ce8aeeeed83743
Alex Vasylenko <o.vasylenko@wookieelabs.com>
2015-11-04 12:01:13 +0200

    Add correct data for search reducer

--- src/reducers/activeSerachResult.js
+++ src/reducers/activeSerachResult.js
@@ -1,8 +1,11 @@
 import * as types from 'actions/types';
 const defaultState = {
   loading: false,
-  entity: {
-    data: new WinJS.Binding.List([]),
+  entities: {
+    data: {
+      media: [],
+      users: [],
+    },
   },
 };
 
@@ -12,24 +15,9 @@ export default function reducer(state = defaultState, action) {
     return { ...state, loading: true, error: null };
   case types.SEARCH_RESULT_LOADED:
     const newEntity = {...action.payload};
-    newEntity.data = new WinJS.Binding.List(action.payload).createGrouped(
-      function(dataItem) {
-        return dataItem.type;
-      },
-      function(dataItem) {
-        return { type: dataItem.type};
-      },
-      function(group1, group2) {
-        if (group1 === 'user') {
-          return -1;
-        } else {
-          return 1;
-        }
-      }
-    );
-
-   // newEntity.media.data = new WinJS.Binding.List(action.payload.media);
-    return { ...state, loading: false, entity: newEntity, error: null };
+    newEntity.users = action.payload.users;
+    newEntity.media = action.payload.media;
+    return { ...state, loading: false, entities: newEntity, error: null };
   case types.SEARCH_RESULT_ERROR:
     return { ...state, loading: false, error: action.payload };
   default:
