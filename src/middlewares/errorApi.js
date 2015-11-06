68741b8dde802df56ccbeb44e0411cadb59e9b44
Alex Vasylenko <o.vasylenko@wookieelabs.com>
2015-11-06 17:18:36 +0200

    Add middlewares for application error

--- src/middlewares/errorApi.js
+++ src/middlewares/errorApi.js
@@ -1,7 +1,13 @@
-import { CALL_API } from '../actions/types';
-
+import { APPLICATION_ERROR } from '../actions/types';
 
 export default store => next => action => {
-  debugger;
-  return next(action);
+
+  if (!action.error) {
+    return next(action);
+  } else {
+    store.dispatch({
+      type: APPLICATION_ERROR,
+    });
+  }
+
 };
