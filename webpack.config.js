6b8429fb7cb71f0dab77f164ecf50fc6b9970854
Alex Vasylenko <o.vasylenko@wookieelabs.com>
2015-11-12 15:13:30 +0200

    create head static file with styles

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,46 @@
 var path = require('path');
 var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 var isProduction = process.env.NODE_ENV === 'production';
 
-var entryPoint = isProduction ? [] : [
-  'webpack-dev-server/client?http://localhost:3000',
-  'webpack/hot/only-dev-server',
-];
-
 module.exports = {
   devtool: isProduction ? 'eval' : 'inline-source-maps',
-  entry: entryPoint.concat([
-    './src/index'
-  ]),
+  entry: [
+    './src/index',
+  ],
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'static'),
     filename: 'bundle.js',
-    publicPath: '/static/'
+    publicPath: '/static/',
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new ExtractTextPlugin('styles.css', {publicPath: '/static/'}),
+    new AssetsPlugin({
+      filename: 'assets.json',
+      path: __dirname + '/static',
+    }),
   ],
   resolve: {
     extensions: ['', '.js', '.jsx'],
-    root: path.join(__dirname, 'src')
+    root: path.join(__dirname, 'src'),
   },
   module: {
     loaders: [{
       test: /\.jsx?$/,
       loaders: ['react-hot', 'babel'],
-      include: path.join(__dirname, 'src')
+      include: path.join(__dirname, 'src'),
     }, {
       test: /\.css$/,
-      loader: "style-loader!css-loader?modules&sourceMap&root=.&localIdentName=[path][name]---[local]---[hash:base64:5]",
-      include: path.join(__dirname, 'src')
-    },{
+      loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&sourceMap&root=.&localIdentName=[path][name]---[local]---[hash:base64:5]'),
+      include: path.join(__dirname, 'src'),
+    }, {
       test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-      loader: "url-loader?limit=10000&minetype=application/font-woff"
+      loader: "url-loader?limit=10000&minetype=application/font-woff",
     }, {
       test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-      loader: "file-loader"
-    }]
-  }
+      loader: "file-loader",
+    }],
+  },
+
 };
