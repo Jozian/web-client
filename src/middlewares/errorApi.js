32761f9e407819c11e03c77471d9e995017fe010
Alex Vasylenko <o.vasylenko@wookieelabs.com>
2015-11-27 17:07:25 +0200

    Fix bug with error middleware

--- src/middlewares/errorApi.js
+++ src/middlewares/errorApi.js
@@ -2,12 +2,11 @@ import { APPLICATION_ERROR } from '../actions/types';
 
 export default store => next => action => {
 
-  if (!action.error) {
-    return next(action);
-  } else {
+  if (action.error && action.errorStatus < 500) {
     store.dispatch({
       type: APPLICATION_ERROR,
     });
+  } else {
+    return next(action);
   }
-
 };
