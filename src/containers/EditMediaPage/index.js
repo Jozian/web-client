b700198f031d59ec5ee6cc17eee2fc1044aa3b1a
Alex Vasylenko <o.vasylenko@wookieelabs.com>
2015-12-08 22:45:34 +0200

    Add correct media upload popup

--- src/containers/EditMediaPage/index.js
+++ src/containers/EditMediaPage/index.js
@@ -7,6 +7,7 @@ import * as actions from 'actions/media';
 import Button from 'components/Button';
 import Footer from 'components/Footer';
 import FormInput from 'components/Form/FormInput';
+import { baseUrl } from '../../api/helper';
 
 import styles from './index.css';
 
@@ -42,7 +43,6 @@ export default class EditMediaPage extends Component {
   }
 
   componentWillReceiveProps(props) {
-
     this.setState({
       activeMedia: props.activeMedia,
       currentMediaName: props.activeMedia.name,
@@ -58,15 +58,11 @@ export default class EditMediaPage extends Component {
     this.context.router.transitionTo('folder', {folderId: this.props.params.folderId});
   }
 
-  onMediaNameInputChange(event) {
+  onMediaInputChange(field, e) {
     this.setState({
       activeMedia: {
-        id: this.props.activeMedia.id,
-        name: event.target.value,
-        type: this.props.activeMedia.type,
-        description: this.props.activeMedia.description,
-        links: this.props.activeMedia.links,
-
+        ...this.state.activeMedia,
+      [field]: e.target.value,
       },
     });
   }
@@ -107,14 +103,20 @@ export default class EditMediaPage extends Component {
     this.props.loadMedia(this.props.params.itemId);
   }
 
+  onUploadFile(e) {
+    if (e.keyCode.toString() === '13') {
+      React.findDOMNode(this.refs.fileInputEditMedia).click();
+    }
+  }
+
   setAllImages(id) {
     return (
       <div>
-        <img className={styles.currentImage} src={'/preview/library' + id + '.png'} />
+        <img className={styles.currentImage} src={`${baseUrl}//preview/${id}.png`} />
 
         <label className={styles.wrapLabel}>
-          <input type="file" name="file" className={styles.inputFile} onChange={::this.handlerUploadImage} />
-          <div className={styles.importContainer} type="button">Upload preview</div>
+          <input type="file" name="file" className={styles.inputFile} onChange={::this.handlerUploadImage} ref="fileInputEditMedia" />
+          <div className={styles.importContainer} role="button" tabIndex="0" onKeyDown={::this.onUploadFile}>Upload preview</div>
         </label>
 
         {this.state.allPreview.map((item) => {
@@ -132,34 +134,39 @@ export default class EditMediaPage extends Component {
           <span className={styles.toolbarTitle} onClick={::this.cancelSave}>{this.state.currentMediaName}</span>
         </div>
           <div className={styles.mediaForm}>
-            <form>
-              <label className={styles.label}>Name</label>
+            <form className={styles.formBody}>
+              <label className={styles.label} htmlFor="inputMediaField">Name</label>
               <input
                 className={styles.inputName}
                 name="name"
                 placeholder="i.e. John Doe"
                 type="text"
+                id="inputMediaField"
                 value={this.state.activeMedia.name}
-                onChange={::this.onMediaNameInputChange}
+                onChange={this.onMediaInputChange.bind(this, 'name')}
                 />
 
-              <label className={styles.typeLabel}>Type:</label>
-              <span className={styles.typeName}>{this.state.activeMedia.type}</span>
+              <label className={styles.typeLabel} htmlFor="mediaType">Type:</label>
+              <span className={styles.typeName} id="mediaType" tabIndex="0">{this.state.activeMedia.type}</span>
 
-              <label className={styles.label}>Description</label>
+              <label className={styles.label} htmlFor="mediaDescription">Description</label>
               <textarea
                 className={styles.descriptionArea}
+                id="mediaDescription"
                 name="description"
                 placeholder="description text"
                 value={this.state.activeMedia.description}
+                onChange={this.onMediaInputChange.bind(this, 'description')}
                 ></textarea><br />
 
-              <label className={styles.label}>External links</label>
+              <label className={styles.label} htmlFor="mediaExternalLinks">External links</label>
               <input
+                id="mediaExternalLinks"
                 className={styles.inputName}
                 name="links"
                 placeholder="links"
                 value={this.state.activeMedia.links}
+                onChange={this.onMediaInputChange.bind(this, 'links')}
                 /><br />
 
               <div>
@@ -171,11 +178,13 @@ export default class EditMediaPage extends Component {
                   disabled={!this.state.activeMedia.name.length}
                   className="mdl2-check-mark"
                   onClick={::this.saveNewMedia}
+                  tooltipText="Save media"
                   >
                 </Button>
                 <Button
                   className="mdl2-cancel"
                   onClick={::this.cancelSave}
+                  tooltipText="Cancel save media"
                   >
                 </Button>
               </Footer>
