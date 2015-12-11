0f1b73ed36ef7c2ef356dc8e2941e2c509267de0
Alex Vasylenko <o.vasylenko@wookieelabs.com>
2015-12-11 18:29:28 +0200

    Add wrapp into libraries names

--- src/common/index.js
+++ src/common/index.js
@@ -8,3 +8,24 @@ export const onEnterPressed = fn => event => {
   }
 };
 
+function returnPart(number, string, newString) {
+  const currentNumber = number * 40;
+  newString = newString + string.slice(currentNumber, currentNumber + 40) + ' \n ';
+
+  if (string.length > currentNumber + 40) {
+    return returnPart(number + 1, string, newString);
+  }else if(string.length < currentNumber + 40 && string.length > currentNumber) {
+    return newString + string.slice(currentNumber);
+  } else {
+    return newString;
+  }
+}
+
+export function wrapLongString(string) {
+  if (string.length > 40) {
+    return returnPart(0, string, '');
+  } else {
+    return string;
+  }
+}
+
--- src/containers/LibrariesPage/index.js
+++ src/containers/LibrariesPage/index.js
@@ -14,6 +14,7 @@ import Button from 'components/Button';
 import Footer from 'components/Footer';
 import WhiteFooter from 'components/WhiteFooter';
 import loading from 'decorators/loading';
+import { wrapLongString } from '../../common';
 
 import commonStyles from 'common/styles.css';
 import styles from './index.css';
@@ -139,7 +140,7 @@ class LibrariesPage extends Component {
 
   renderName(_, rowData) {
     if (!this.state.editLibrary || this.state.editLibrary.id !== rowData.id) {
-      return rowData.name;
+      return wrapLongString(rowData.name);
     } else {
       return (
         <div onClick={ (e) => e.stopPropagation() }>
