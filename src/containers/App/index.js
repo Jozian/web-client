ff39cf9c35d000f26e2fc52a4a0cb65486964a6b
Alex Vasylenko <o.vasylenko@wookieelabs.com>
2015-12-17 12:00:53 +0200

    Add new modal for client error

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 import * as actions from '../../actions/users';
+import * as appActions from '../../actions/app';
 import Modal from 'components/Modal';
 import NavBar from 'components/NavBar';
 import ToastManager from '../ToastManager';
@@ -14,22 +15,70 @@ import styles from './index.css';
 import commonStyles from 'common/styles.css';
 
 @connect(
-  (state) => ({user: state.currentUser, error: state.errorApplication.error}),
-  (dispatch) => bindActionCreators(actions, dispatch)
+  (state) => ({user: state.currentUser, error: state.errorApplication.error, clientError: state.clientError}),
+  (dispatch) => bindActionCreators(Object.assign({}, actions, appActions), dispatch)
 )
 export default class App extends Component {
   static propTypes = {
     user: React.PropTypes.object.isRequired,
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      openClientError: false,
+      errorClientText: '',
+    };
+  }
+
   componentWillReceiveProps(props) {
     if (!props.user.token) {
       window.location = '/index.html';
+    }
+
+    if (props.clientError.hasError && props.clientError.errorStatus === 403) {
+      props.logoutUser();
+    } else if (props.clientError.hasError && props.clientError.errorStatus === 404) {
+      this.setState({
+        openClientError: true,
+        errorClientText: 'The server was not able to find the data you have provided. Perhaps this particular data is outdated. After you press the button below, you can continue working with the application.',
+      });
+    } else if (props.clientError.hasError && props.clientError.errorStatus === 400) {
+      this.setState({
+        openClientError: true,
+        errorClientText: 'The server was not able to process the data you have provided. After you click the button below, you an try again with different data.',
+      });
     }
   }
 
   reloadPage() {
     window.location.reload();
+  }
+
+  hideClientPopup() {
+    this.setState({
+      openClientError: false,
+    });
+
+    this.props.hideClientError();
+  }
+
+  renderClientErrorPopup() {
+    return (<Modal
+      isOpen={this.state.openClientError}
+      title="Client error."
+      className={commonStyles.modal}
+      >
+      <div className={styles.popupText}>{this.state.errorClientText}</div>
+      <WhiteFooter>
+        <ActionButtonForModal
+          className={commonStyles.saveButtonModal}
+          onClick={::this.hideClientPopup}
+          >
+          Hide
+        </ActionButtonForModal>
+      </WhiteFooter>
+    </Modal>);
   }
 
   renderErrorPopup() {
@@ -59,6 +108,7 @@ export default class App extends Component {
   render() {
     return (<div>
       { this.renderErrorPopup() }
+      { this.renderClientErrorPopup() }
 
       <ToastManager />
 
