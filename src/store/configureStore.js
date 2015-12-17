04786c5e2f4fe562f80fa87b00799f39090e1cdf
Alex Vasylenko <o.vasylenko@wookieelabs.com>
2015-12-17 12:03:30 +0200

    Add field into store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -24,6 +24,7 @@ const createFinalStore = compose(
 const store = createFinalStore(reducer, {
   currentUser: null,
   errorApplication: null,
+  clientError: {},
   statistics: {
     loading: false,
     entities: {
