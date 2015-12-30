ad6ded6ba8145487e21d259c9d4f54620dbb10ff
Alex Vasylenko <o.vasylenko@wookieelabs.com>
2015-12-30 10:40:56 +0200

    Fix bug with norrator for dropdown

--- src/components/Dropdown/index.js
+++ src/components/Dropdown/index.js
@@ -3,26 +3,28 @@ import styles from './index.css';
 import fontAwesome from 'font-awesome-webpack'; // eslint-disable-line no-unused-vars
 
 export default class Dropdown extends Component {
-    static propTypes = {
-      title: PropTypes.string,
-      onChange: PropTypes.func,
-      disabled: PropTypes.bool,
-      value: PropTypes.string,
-    };
+  static propTypes = {
+    title: PropTypes.string,
+    onChange: PropTypes.func,
+    disabled: PropTypes.bool,
+    value: PropTypes.string,
+  };
 
-    render() {
-        return (
-            <div className={styles.editUser}>
-                <label className={styles.userLabel}>{this.props.title}</label>
-                <select className={styles.optionsSelect} name="type"
-                        disabled={this.props.disabled}
-                        value={this.props.value}
-                        onChange={this.props.onChange}
-                        role="listbox"
-                        required>
-                    {this.props.children}
-                </select>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className={styles.editUser}>
+        <label htmlFor="selectUserRole" className={styles.hideLabel}>Account type user:</label>
+        <label className={styles.userLabel}>{this.props.title}</label>
+        <select className={styles.optionsSelect} name="type"
+                disabled={this.props.disabled}
+                value={this.props.value}
+                onChange={this.props.onChange}
+                role="listbox"
+                id="selectUserRole"
+                required>
+            {this.props.children}
+        </select>
+      </div>
+    );
+  }
 }
