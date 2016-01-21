e93587d77b37791b0b2ae1ce9f29d31b8e5dbd43
Alex Vasylenko <o.vasylenko@wookieelabs.com>
2016-01-21 11:49:56 +0200

    Add new line for list manager

--- src/decorators/listManager.js
+++ src/decorators/listManager.js
@@ -1,3 +1,66 @@
-/**
- * Created by alex on 20.01.16.
- */
+import React, { Component } from 'react';
+import { eq } from 'lodash';
+import { diff } from 'adiff';
+
+const wrapper = (lists) => (WrappedComponent) => {
+  class ListManager extends Component {
+
+    constructor(props) {
+      super(props);
+      this.state = {};
+      lists.forEach(key => {
+        this.state[key] = new WinJS.Binding.List(props[key]);
+      });
+    }
+
+    componentWillReceiveProps(newProps) {
+      const newState = {};
+      console.time('doing shit');
+      lists.forEach(key => {
+        const data = this.props[key];
+        const list = this.state[key];
+        const newData = newProps[key];
+        const newDataMap = {};
+        newData.forEach(item =>
+            newDataMap[item.$id] = item
+        );
+
+        if (data !== newData) {
+          if (!list) {
+            newState[key] = new WinJS.Binding.List(newData);
+          } else {
+            diff(
+              data.map(item => item.$id),
+              newData.map(item => item.$id)
+            ).map(diffItem =>
+                diffItem.map((id, idx) =>
+                    idx < 2 ? id : newDataMap[id]
+                )
+            ).forEach(diffItem =>
+                list.splice.apply(list, diffItem)
+            );
+
+            list.forEach((item, idx) => {
+              if (!eq(item, newDataMap[item.$id])) {
+                this.state[key].setAt(idx, newDataMap[item.$id]);
+              }
+            });
+          }
+        }
+      });
+
+      console.timeEnd('doing shit');
+      if (Object.keys(newState)) {
+        this.setState(newState);
+      }
+    }
+
+    render() {
+      return <WrappedComponent {...this.props} {...this.state} />;
+    }
+  }
+  ListManager.dispayName = `WinJSListManager(${WrappedComponent.displayName})`;
+  return ListManager;
+};
+
+export default wrapper;
