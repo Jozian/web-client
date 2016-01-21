d2f33b17c495c29034dc62aeef0c9273db254662
Alex Vasylenko <o.vasylenko@wookieelabs.com>
2016-01-21 11:47:28 +0200

    Add new case for media uploaded into reducer

--- src/reducers/activeFolder.js
+++ src/reducers/activeFolder.js
@@ -2,7 +2,7 @@ import * as types from 'actions/types';
 const defaultState = {
   loading: false,
   entity: {
-    data: new WinJS.Binding.List([]),
+    data: [],
   },
 };
 
@@ -11,11 +11,30 @@ export default function reducer(state = defaultState, action) {
   case types.FOLDER_LOADING:
     return { ...state, loading: true, error: null };
   case types.FOLDER_LOADED:
-    const newEntity = {...action.payload};
-    newEntity.data = new WinJS.Binding.List(action.payload.data);
+    const newEntity = {};
+    newEntity.data = action.payload.data.map(item => {
+      return {
+        ...item,
+        $id: item.type + item.id,
+        timeStamp: new Date().getTime().toString(),
+      };
+    });
+    newEntity.name = action.payload.name;
+    newEntity.path = action.payload.path;
     return { ...state, loading: false, entity: newEntity, error: null };
   case types.FOLDER_LOAD_ERROR:
     return { ...state, loading: false, error: action.payload };
+  case types.PREV_IMAGE_UPLOADED:
+    const newEntityAfterImageUpdate = {};
+    const indexItem = state.entity.data.findIndex((item) => `//preview/${item.id}.png` === action.payload);
+    const changeItem = {...state.entity.data[indexItem], timeStamp: new Date().getTime().toString()};
+
+    newEntityAfterImageUpdate.data = [...state.entity.data.slice(0, indexItem),
+      changeItem,
+      ...state.entity.data.slice(indexItem + 1)];
+    newEntityAfterImageUpdate.name = state.entity.name;
+    newEntityAfterImageUpdate.path = [...state.entity.path];
+    return { ...state, loading: false, entity: newEntityAfterImageUpdate, error: null };
   default:
     return state;
   }
